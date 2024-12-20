5e820229d1962b52cf7eedabff0cf066cec83a74
Mohamed_Shosha <shoshamohamed313@gmail.com>
2024-12-20 15:41:11 +0200

    Add initial project files

--- Q_A.API/obj/Debug/net8.0/Q_A.API.AssemblyInfo.cs
+++ Q_A.API/obj/Debug/net8.0/Q_A.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Q_A.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2ec883dc77c98e59e38824a9306222ed6c0984f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Q_A.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Q_A.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
